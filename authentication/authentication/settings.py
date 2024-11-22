75b829f1ff0ae226f60cc150a3ec9e5bf6684a97
iota07 <hanesananhouvannasak@googlemail.com>
2024-11-22 14:35:43 +0100

    changed restframework authentication back to token

--- authentication/authentication/settings.py
+++ authentication/authentication/settings.py
@@ -114,11 +114,6 @@ EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-
-# Default email settings
-
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
-
 # Django REST Framework settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
