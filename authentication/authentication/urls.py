412a65aba295fe345e9c65490adce424ba7fccd9
iota07 <hanesananhouvannasak@googlemail.com>
2024-11-22 11:38:37 +0100

    removed custom login

--- authentication/authentication/urls.py
+++ authentication/authentication/urls.py
@@ -16,12 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import CustomLoginView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('api/auth/login/', CustomLoginView.as_view(), name='account_login'),
     path('api/auth/', include('dj_rest_auth.urls')),
     path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
