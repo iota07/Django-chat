2408432cde85cdd7e78f6aa17bafa94a49dd6f05
iota07 <hanesananhouvannasak@googlemail.com>
2024-11-28 18:22:41 +0100

    fix custom view in url

--- authentication/authentication/urls.py
+++ authentication/authentication/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from allauth.account.views import confirm_email
 from .views import CustomLoginView, CustomConfirmEmailView
 
 schema_view = get_schema_view(
