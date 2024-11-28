9c11733295b04599de27f31b4a2576746f012efd
iota07 <hanesananhouvannasak@googlemail.com>
2024-11-28 16:43:34 +0100

    fix login views to accomodate JSON data

--- authentication/authentication/views.py
+++ authentication/authentication/views.py
@@ -5,19 +5,24 @@ from django.utils.decorators import method_decorator
 from django.views import View
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+import json
 
 @method_decorator(csrf_exempt, name='dispatch')
 class CustomLoginView(View):
     def post(self, request, *args, **kwargs):
-        email = request.POST.get('email')
-        password = request.POST.get('password')
         try:
-            user = User.objects.get(email=email)
-            user = authenticate(request, username=user.username, password=password)
-            if user is not None:
-                login(request, user)
-                return JsonResponse({'detail': 'Logged in successfully'})
-            else:
+            data = json.loads(request.body)
+            email = data.get('email')
+            password = data.get('password')
+            try:
+                user = User.objects.get(email=email)
+                user = authenticate(request, username=user.username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return JsonResponse({'detail': 'Logged in successfully'})
+                else:
+                    return JsonResponse({'detail': 'Invalid credentials'}, status=400)
+            except User.DoesNotExist:
                 return JsonResponse({'detail': 'Invalid credentials'}, status=400)
-        except User.DoesNotExist:
-            return JsonResponse({'detail': 'Invalid credentials'}, status=400)
+        except json.JSONDecodeError:
+            return JsonResponse({'detail': 'Invalid JSON'}, status=400)
