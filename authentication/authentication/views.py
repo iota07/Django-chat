95a3f27c31c731e84775baa66e4537da56ed80cd
iota07 <hanesananhouvannasak@googlemail.com>
2024-11-28 16:33:56 +0100

    changed username to email

--- authentication/authentication/views.py
+++ authentication/authentication/views.py
@@ -4,15 +4,20 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 
 @method_decorator(csrf_exempt, name='dispatch')
 class CustomLoginView(View):
     def post(self, request, *args, **kwargs):
-        username = request.POST.get('username')
+        email = request.POST.get('email')
         password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return JsonResponse({'detail': 'Logged in successfully'})
-        else:
+        try:
+            user = User.objects.get(email=email)
+            user = authenticate(request, username=user.username, password=password)
+            if user is not None:
+                login(request, user)
+                return JsonResponse({'detail': 'Logged in successfully'})
+            else:
+                return JsonResponse({'detail': 'Invalid credentials'}, status=400)
+        except User.DoesNotExist:
             return JsonResponse({'detail': 'Invalid credentials'}, status=400)
